35709004ddcb489217e9bc567b9eeba3cb39b96e
cytoni <113114304+cytoni@users.noreply.github.com>
2022-10-04 15:39:12 +0200

    update

--- src/App.js
+++ src/App.js
@@ -13,24 +13,26 @@ function App() {
   }, [aufgabe]);
 
   //Eine Liste mit React machen
-  const todoListe = todos.map((item) => <li key={item.id}>{item.todo}</li>);
+  const todoListe = todos.map((item) => <li key={item.id}>{item.todo} <button id={item.id} onClick={handleDelete}>Löschen</button></li>);
   
+  function handleDelete(event) {
+    const id = event.target.id;
+    axios.delete(`localhost:3004/todos/${event.target.id}`)
+  }
+
   //EventHandler Funktion für das Submit Event
   function handleSubmit(event){
     event.preventDefault();
     //In die Datenbank schreiben 
     axios.post("http://localhost:3004/todos", { todo: aufgabe })
-    .then(response) => {setAufgabe("")};
-    console.log(response);
-    //Eingabefeld leeren
-    setAufgabe("");
+    .then((response) => {setAufgabe("")});
   }
   return (
   <div className="App">
     <h1>Meine ToDo Liste</h1>
     <form onSubmit={handleSubmit}>
       {/* Value muss einer Variable entsprechen */}
-      <input type="text" name="aufgabe" value={aufgabe} onChange={(event) => setAufgabe(event.target.value)} />
+      <input required={true} type="text" name="aufgabe" value={aufgabe} onChange={(event) => setAufgabe(event.target.value)} />
       <button type="submit">Senden</button>
     </form>
   <ul>{todoListe}</ul>
