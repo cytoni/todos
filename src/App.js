99d5f285400b10ef3266a09c32672ab0913ed5c6
cytoni <113114304+cytoni@users.noreply.github.com>
2022-10-04 15:16:04 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,19 +3,36 @@ import axios from "axios";
 import { useEffect, useState } from 'react';
 
 function App() {
+  //useState
   const [todos, setTodos] = useState([]);
+  const [aufgabe, setAufgabe] = useState("");
 
   useEffect(() => {
     //Listen Daten von der REST Api holen
-    axios.get("http://localhost:3000/todos").
-    then((response) => setTodos(response.data));
-  }, []);
+    axios.get("http://localhost:3004/todos").then((response) => setTodos(response.data));
+  }, [aufgabe]);
 
   //Eine Liste mit React machen
-  const todoListe = todos.map((item) => <li key={item.id}>{item.todo}</li>)
+  const todoListe = todos.map((item) => <li key={item.id}>{item.todo}</li>);
+  
+  //EventHandler Funktion für das Submit Event
+  function handleSubmit(event){
+    event.preventDefault();
+    //In die Datenbank schreiben 
+    axios.post("http://localhost:3004/todos", { todo: aufgabe })
+    .then(response) => {setAufgabe("")};
+    console.log(response);
+    //Eingabefeld leeren
+    setAufgabe("");
+  }
   return (
   <div className="App">
     <h1>Meine ToDo Liste</h1>
+    <form onSubmit={handleSubmit}>
+      {/* Value muss einer Variable entsprechen */}
+      <input type="text" name="aufgabe" value={aufgabe} onChange={(event) => setAufgabe(event.target.value)} />
+      <button type="submit">Senden</button>
+    </form>
   <ul>{todoListe}</ul>
   </div>);
 }
